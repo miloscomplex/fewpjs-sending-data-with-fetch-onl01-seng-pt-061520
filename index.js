5a2fe1bbbb042c1668150261d6b6d1bf792e6db6
miloscomplex <hicallmesutton@gmail.com>
2021-01-05 01:01:39 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+let container = document.querySelector('div.container')
+
+function submitData(userName, userEmail) {
+  let data = {
+    name: userName,
+    email: userEmail
+  }
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      document.body.innerHTML = object["id"].toString()
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message
+    })
+}
